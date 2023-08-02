a01f884e28a4ee51266b8ef0b419b9b0db4f1957
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-02 23:14:15 +0530

    Locomotive Scroll Elements Disappear Fixed

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-});
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("#main"),
+//   smooth: true,
+// });
+
+(function () {
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+    smoothMobile: false,
+  });
+  new ResizeObserver(() => scroll.update()).observe(
+    document.querySelector("#main")
+  );
+})();
