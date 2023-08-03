5bc6870e5775db4420764495fcad1659d6f5b8cd
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-08-03 10:47:10 +0530

    Hero Section Animated

--- script.js
+++ script.js
@@ -13,3 +13,27 @@
     document.querySelector("#main")
   );
 })();
+
+(function heroAnimation() {
+  var tl = gsap.timeline();
+
+  tl.from("#nav", {
+    y: -10,
+    duration: 1.3,
+    opacity: 0,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingElement", {
+      delay: -0.9,
+      y: 0,
+      duration: 2,
+      ease: Expo.easeInOut,
+      stagger: 0.2,
+    })
+    .from("#heroFooter", {
+      opacity: 0,
+      delay: -1.2,
+      duration: 1.5,
+      ease: Expo.easeInOut,
+    });
+})();
